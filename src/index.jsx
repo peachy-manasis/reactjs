283cd8c128aa3e27211f8e54bd3dde75d83d69b0
peachy-manasis <peachy.manasis@crmworks.asia>
2017-04-01 05:36:41 +0800

    webpack
    - used npm and node to serve up test SPA with dynamically-generated bundle.js

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HenloFren = () => {
+	return (
+	<div>Henlo Fren</div>
+	)
+}
+
+ReactDOM.render(
+	<HenloFren />,
+	document.getElementById('app')
+);
